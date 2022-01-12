46ec77c9f80209bc08db58d5e53bf09df1425be2
Iuri Martins Carvalho <iuri@vixsystem.com.br>
2022-01-12 17:46:36 -0300

    Teste de carga: cartografia.ponto_cotado

---
+++ fontes/etl_carga/sql_scripts/ponto_cotado.sql
@@ -0,0 +1,12 @@
+drop table if exists cartografia.ponto_cotado;
+
+create table if not exists cartografia.ponto_cotado (
+    id bigserial primary key,
+    elevacao numeric (38, 8) null ,
+    data date null ,
+    geometry geometry('POINT', 31984)
+);
+
+truncate table cartografia.ponto_cotado;
+
+create index if not exists ponto_cotado_geometry_idx on cartografia.ponto_cotado using GIST(geometry);
