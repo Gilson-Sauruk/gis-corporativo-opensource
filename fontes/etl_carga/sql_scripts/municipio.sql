d2a18ff46eb721060547eedfc368f3be3516d531
Iuri Martins Carvalho <iuri@vixsystem.com.br>
2022-01-12 16:06:27 -0300

    Teste de carga: cartografia.municipio

--- fontes/etl_carga/sql_scripts/municipio.sql
+++ fontes/etl_carga/sql_scripts/municipio.sql
@@ -1,22 +1,22 @@
 drop table if exists cartografia.municipio;
+
 create table if not exists cartografia.municipio (
-	id bigserial primary key,
-	nome varchar(30) not null,
-	area_ibge numeric(38,8) null,
-	data_criac date null,
-	populacao bigint null,
-	data date null,
-	codigo_sicat int null,
-	codigo_divisao int null,
-	codigo_sap int null,
-	tipo_servico varchar(30),
-	cod_uf  int null,
-	codigo_ibge int null,
-	ano_ref date null,
-    geometry geometry('MULTIPOLYGON', 31984)
+    id bigserial primary key
+    nome varchar (30) null 
+    area_ibge numeric (38, 8) null 
+    data_criac date null 
+    populacao numeric (10, 0) null 
+    data date null 
+    codigo_sicat numeric (10, 0) null 
+    codigo_divisao numeric (10, 0) null 
+    codigo_sap numeric (10, 0) null 
+    tipo_servico varchar (30) null 
+    cod_uf numeric (38, 8) null 
+    codigo_ibge numeric (10, 0) null 
+    ano_ref date null 
+    geometry geometry('POLYGON', 31984)
 );
 
 truncate table cartografia.municipio;
 
-create index if not exists nome_idx on cartografia.municipio(nome);
-create index if not exists municipio_geometry_idx on cartografia.municipio using GIST (geometry);
+create index if not exists municipio_geometry_idx on cartografia.municipio using GIST(geometry);
