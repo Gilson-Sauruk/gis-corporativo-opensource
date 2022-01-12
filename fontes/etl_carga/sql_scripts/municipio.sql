e1188971e83448a2f68ff21d8141eee0d531d030
Iuri Martins Carvalho <iuri@vixsystem.com.br>
2022-01-12 16:09:48 -0300

    Teste de carga: cartografia.municipio

--- fontes/etl_carga/sql_scripts/municipio.sql
+++ fontes/etl_carga/sql_scripts/municipio.sql
@@ -1,19 +1,19 @@
 drop table if exists cartografia.municipio;
 
 create table if not exists cartografia.municipio (
-    id bigserial primary key
-    nome varchar (30) null 
-    area_ibge numeric (38, 8) null 
-    data_criac date null 
-    populacao numeric (10, 0) null 
-    data date null 
-    codigo_sicat numeric (10, 0) null 
-    codigo_divisao numeric (10, 0) null 
-    codigo_sap numeric (10, 0) null 
-    tipo_servico varchar (30) null 
-    cod_uf numeric (38, 8) null 
-    codigo_ibge numeric (10, 0) null 
-    ano_ref date null 
+    id bigserial primary key,
+    nome varchar (30) null ,
+    area_ibge numeric (38, 8) null ,
+    data_criac date null ,
+    populacao numeric (10, 0) null ,
+    data date null ,
+    codigo_sicat numeric (10, 0) null ,
+    codigo_divisao numeric (10, 0) null ,
+    codigo_sap numeric (10, 0) null ,
+    tipo_servico varchar (30) null ,
+    cod_uf numeric (38, 8) null ,
+    codigo_ibge numeric (10, 0) null ,
+    ano_ref date null ,
     geometry geometry('POLYGON', 31984)
 );
 
