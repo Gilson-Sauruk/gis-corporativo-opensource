ad8f86e9aa43b727bbafc01ebb547161e75e60c8
Iuri Martins Carvalho <iuri@vixsystem.com.br>
2022-01-13 13:53:11 -0300

    Teste de carga: cartografia.sed_destrital

---
+++ fontes/etl_carga/sql_scripts/sede_distrital.sql
@@ -0,0 +1,12 @@
+drop table if exists cartografia.sede_distrital;
+
+create table if not exists cartografia.sede_distrital (
+    id serial primary key,
+    nome varchar (30) nullnot null ,
+    data numeric (38, 8) null ,
+    geometry geometry('POINT', 31984)
+);
+
+truncate table cartografia.sede_distrital;
+
+create index if not exists sede_distrital_geometry_idx on cartografia.sede_distrital using GIST(geometry);
