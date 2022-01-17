77c7232a167e1586317b605307b44388923018af
Iuri Martins Carvalho <iuri@vixsystem.com.br>
2022-01-17 12:10:48 -0300

    Teste de carga: curva_de_nivel_mestra_10x10

---
+++ fontes/etl_carga/sql_scripts/curva_de_nivel_mestra_10x10.sql
@@ -0,0 +1,12 @@
+drop table if exists cartografia.curva_de_nivel_mestra_10x10;
+
+create table if not exists cartografia.curva_de_nivel_mestra_10x10 (
+    id serial primary key,
+    data varchar (254) null ,
+    elevacao numeric (38, 8) null ,
+    geometry geometry('MULTILINESTRING', 31984)
+);
+
+truncate table cartografia.curva_de_nivel_mestra_10x10;
+
+create index if not exists curva_de_nivel_mestra_10x10_geometry_idx on cartografia.curva_de_nivel_mestra_10x10 using GIST(geometry);
