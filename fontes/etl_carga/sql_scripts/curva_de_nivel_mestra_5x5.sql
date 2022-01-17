4596364dfc167ffb74694b5c59c76c1f24f862e5
Gilson Sauruk <gilson@vixsystem.com.br>
2022-01-17 12:01:52 -0300

    Teste carga feição : curva_de_nivel_mestra_5x5

---
+++ fontes/etl_carga/sql_scripts/curva_de_nivel_mestra_5x5.sql
@@ -0,0 +1,12 @@
+drop table if exists cartografia.curva_de_nivel_mestra_5x5;
+
+create table if not exists cartografia.curva_de_nivel_mestra_5x5 (
+    id serial primary key,
+    elevacao numeric (10, 0) null ,
+    data date null ,
+    geometry geometry('MULTILINESTRING', 31984)
+);
+
+truncate table cartografia.curva_de_nivel_mestra_5x5;
+
+create index if not exists curva_de_nivel_mestra_5x5_geometry_idx on cartografia.curva_de_nivel_mestra_5x5 using GIST(geometry);
